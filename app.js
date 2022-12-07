bd2e41389a309de0c5a83f5dce1863eb91730f88
Sai Srinivas <samanthula.sai@gmail.com>
2022-12-07 16:29:40 +0530

    Updated the message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Checking how the commit works'));
+app.get('/', (req, res) => res.send('Checking how the commit works | Now It the final review stage of cd pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
